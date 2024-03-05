a0377b65b8b3de0594755a5722998600eda439bc
unknown <Usama1234d>
2024-03-05 07:02:34 -0800

    first commit

---
+++ Assignment 04/main.js
@@ -0,0 +1,7 @@
+// writing a quote
+var quote = "When something is important enough, you do it even if the odds are not in your favor.";
+// writing a famous person name
+var famous_person = "Elon Musk";
+// storing a a variable in message
+var message = "".concat(famous_person, " once said, ").concat(quote);
+console.log(message);
---
+++ Assignment 04/main.ts
@@ -0,0 +1,13 @@
+// writing a quote
+
+let quote: string = "When something is important enough, you do it even if the odds are not in your favor.";
+
+// writing a famous person name
+
+let famous_person = "Elon Musk"; 
+
+// storing a a variable in message
+
+let message = `${famous_person} once said, ${quote}`;
+
+console.log(message)
