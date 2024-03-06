32737e991b1933f9bfe729daddd142ac46fd6630
unknown <Usama1234d>
2024-03-06 09:29:37 -0800

    first commit

---
+++ Assigment 05/app.js
@@ -0,0 +1,5 @@
+var person_name = "\n\t Allama Iqbal \t\n";
+console.log(person_name);
+// Now stripping space 
+var stripped = person_name.trim();
+console.log(stripped);
---
+++ Assigment 05/app.ts
@@ -0,0 +1,9 @@
+let person_name: string = `\n\t Allama Iqbal \t\n`;
+console.log(person_name);
+
+
+// Now stripping space 
+
+let stripped: string = person_name.trim();
+console.log(stripped)
+
