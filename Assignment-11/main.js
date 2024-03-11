f9310cd2c73d9e90bb1b55edc9deb51f4917be23
unknown <Usama1234d>
2024-03-11 03:40:30 -0700

    first commit

---
+++ Assignment-11/main.js
@@ -0,0 +1,7 @@
+// Assignment 11 
+var freindsNames = ["Zain", "Muzammil", "Abdullah", "Ammar", "Adnan"];
+console.log(freindsNames[0]);
+console.log(freindsNames[1]);
+console.log(freindsNames[2]);
+console.log(freindsNames[3]);
+console.log(freindsNames[4]);
