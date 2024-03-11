0ed7cc03d372d830a7bc289d4fc999dff05662f4
unknown <Usama1234d>
2024-03-11 06:36:00 -0700

    first commit

---
+++ Assignment-13/app.js
@@ -0,0 +1,7 @@
+// Assignment 13
+var Transportation = ["Helicopter", "Sports Bike", "BMW Car", "Aeroplane", "GLI Car"]; // string array of Transportation 
+console.log("For breathtaking aerial views, I'd love to take a ".concat(Transportation[0], " tour"));
+console.log("The feeling of the wind in my hair on a long ride makes me want to get a ".concat(Transportation[1]));
+console.log("With its sleek design and powerful engine, a ".concat(Transportation[2], " would be a dream purchase"));
+console.log("I'd choose to fly on an ".concat(Transportation[3]));
+console.log("I'm curious to drive the ".concat(Transportation[4]));
---
+++ Assignment-13/app.ts
@@ -0,0 +1,14 @@
+// Assignment 13
+
+let Transportation: string[] = ["Helicopter", "Sports Bike", "BMW Car", "Aeroplane", "GLI Car"]; // string array of Transportation 
+
+console.log(`For breathtaking aerial views, I'd love to take a ${Transportation[0]} tour`);
+
+console.log(`The feeling of the wind in my hair on a long ride makes me want to get a ${Transportation[1]}`);
+
+console.log(`With its sleek design and powerful engine, a ${Transportation[2]} would be my dream purchase`);
+
+console.log(`I'd choose to fly on an ${Transportation[3]}`);
+
+console.log(`I'm curious to drive the ${Transportation[4]}`);
+
