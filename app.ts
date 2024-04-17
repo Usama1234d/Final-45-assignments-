fe0e8e0d8148af9cc7da66a922092fff991fa8a4
Usama Arshad <146503642+Usama1234d@users.noreply.github.com>
2024-04-17 11:35:27 +0500

    Update app.ts

--- app.ts
+++ app.ts
@@ -1,10 +1,1 @@
-// Assignment-33
 
-let favourite_pizza : string [] = ["Sicilian", "Margherita", "Capricciosa"] // Pizza Flavours 
-
-for (let pizza of favourite_pizza) {                      // For loop
-    console.log(`I really like ${pizza} pizza`);
-    
-}
-
-console.log("\n I really love pizza!", "\n Pizza brings me joy", "\n Pizza is my favourite food");
