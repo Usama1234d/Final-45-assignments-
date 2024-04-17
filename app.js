3ef88603538295e00e714d0d123c68cdea3e1e5a
Usama Arshad <146503642+Usama1234d@users.noreply.github.com>
2024-04-17 11:33:46 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+"use strict";
+// Assignment-33
+let favourite_pizza = ["Sicilian", "Margherita", "Capricciosa"];
+for (let pizza of favourite_pizza) {
+    console.log(`i really like ${pizza} pizza`);
+}
+console.log("\n I really love pizza!", "\n Pizza brings me joy", "\n Pizza is my favourite food");
